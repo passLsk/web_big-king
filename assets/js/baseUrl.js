17308b1310e84d28d2afbcf53c8114d6d3fda559
passLsk <471627950@qq.com>
2022-05-20 13:55:36 +0800

    修改了身份获取失败强制跳转到登录页面的bug

--- assets/js/baseUrl.js
+++ assets/js/baseUrl.js
@@ -13,8 +13,9 @@ $(function() {
     }
     //控制用户的访问权限
     option.complete = res => {
+      console.log(res);
       // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-      if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败! ") {
+      if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
         //强制清空token
         localStorage.removeItem("token")
         // 跳转到登录页面
